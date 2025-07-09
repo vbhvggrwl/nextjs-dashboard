03ee12fe9af1023958eedb87d2b4b96b6e7ae3f8
Vaib Aggarwal <va87@cornell.edu>
2025-07-09 11:23:12 +0100

    Acme updates

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/font';
 //import styles from '@/app/ui/home.module.css';
+import Image from 'next/image';
+
 
 export default function Page() {
   return (
@@ -10,6 +12,7 @@ export default function Page() {
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         { <AcmeLogo />}
       </div>
+      
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"/>
@@ -27,8 +30,22 @@ export default function Page() {
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
         </div>
+        
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+          src="/hero-desktop.png"
+          width={1000}
+          height={760}
+          className="hidden md:block"
+          alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+          src="/hero-mobile.png"
+          width={560}
+          height={620}
+          className="block md:hidden"
+          alt="Screenshots of the dashboard project showing mobile version"
+          /> 
         </div>
       </div>
     </main>
